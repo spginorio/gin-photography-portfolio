8714f61a551a6194b85315ea9e6cc1180b0cfa03
spginorio <spginorio@gmail.com>
2025-06-29 17:33:57 +0900

    ready for deloy on netlify

--- src/components/Portraits-images.tsx
+++ src/components/Portraits-images.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const ImageGalleryPortraits = () => {
   const totalImages = 60;
 
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,19 +1,17 @@
-import React from "react";
-
 function About() {
   return (
     <div
       style={{ fontFamily: "'open sans', sans-serif" }}
-      className=" flex flex-col text-[18px] text-gray-800 items-center justify-center mt-36"
+      className="flex flex-col justify-center items-center mt-36 text-[18px] text-gray-800"
     >
       <p className="">📍Osaka</p>
-      <p className="underline mt-6 mb-6">
+      <p className="mt-6 mb-6 underline">
         <a href="mailto:ginoriops@gmail.com">ginoriops@gmail.com</a>
       </p>
       <p>1 305-923-6920</p>
       <a
         href="https://www.instagram.com/ginoriox"
-        className="mt-12 border px-8 py-1 rounded-md"
+        className="mt-12 px-8 py-1 border rounded-md"
       >
         Instagram
       </a>

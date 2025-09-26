9d3fabb0ed51cfb903bbe0ea1c3c4fc5776c12d4
spginorio <spginorio@gmail.com>
2025-09-26 15:37:15 +0900

    updated streets images to match the size of portraits

--- src/components/Street-images.tsx
+++ src/components/Street-images.tsx
@@ -2,27 +2,26 @@ const ImageGallery = () => {
   const totalImages = 46;
 
   return (
-    <div className="gap-10 grid grid-cols-1 sm:grid-cols-2 mx-4 sm:mx-8 lg:mx-60 mt-10">
-      {Array.from({ length: totalImages }, (_, i) => {
-        const imageNumber = i + 1;
-        return (
-          <div
-            key={imageNumber}
-            className="flex justify-center items-start mb-8 sm:mb-14"
-          >
-            <img
-              src={`/streets/${imageNumber}.jpg`}
-              alt={`Street view ${imageNumber}`}
-              className="w-full max-w-full h-auto max-h-[80vh] object-contain"
-              loading="lazy"
-              onError={(e) => {
-                e.currentTarget.src = "/placeholder.jpg";
-                e.currentTarget.alt = "Failed to load image";
-              }}
-            />
-          </div>
-        );
-      })}
+    <div className="">
+      <div className="gap-6 lg:gap-10 space-y-8 sm:space-y-8 lg:space-y-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 mt-10 p-5 sm:p-6 md:p-6 lg:pt-15 lg:pr-15 lg:pl-15 sp-4">
+        {Array.from({ length: totalImages }, (_, i) => {
+          const imageNumber = i + 1;
+          return (
+            <div key={imageNumber} className="group break-inside-avoid">
+              <img
+                src={`/streets/${imageNumber}.jpg`}
+                alt={`Street view ${imageNumber}`}
+                className="w-135 object-cover hover:scale-105 transition-transform duration-300"
+                loading="lazy"
+                onError={(e) => {
+                  e.currentTarget.src = "/placeholder.jpg";
+                  e.currentTarget.alt = "Failed to load image";
+                }}
+              />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
